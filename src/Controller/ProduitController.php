7e48df0d99550f5d14421bd1cf456e3f622ce947
Simon Belhassen <simonbelhassen4@gmail.com>
2021-12-14 15:18:39 +0100

    remit produit dans le controler

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -13,7 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/produit')]
 class ProduitController extends AbstractController
 {
-    #[Route('/', name: 'produit_index', methods: ['GET'])]
+    #[Route('/produit', name: 'produit_index', methods: ['GET'])]
     public function index(ProduitRepository $produitRepository): Response
     {
         return $this->render('produit/index.html.twig', [
